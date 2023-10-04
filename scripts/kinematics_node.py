d83c40f67e46e59c5ee0227810f9dc1b31210e0f
Yikai <yikai.zeng@tu-dresden.de>
2023-10-04 15:38:50 +0200

    add scripts

---
+++ scripts/kinematics_node.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from vlpduckie_base_control.msg import WheelsCmd
+from geometry_msgs.msg import Twist
+
+class KinematicsNode():
+    """
+    The `KinematicsNode` maps car commands send from various nodes to wheel commands that the robot can execute.
+    `KinematicsNode` utilises the car geometry as well as a number of tunining and limiting
+    parameters to calculate the wheel commands that the wheels should execute in order for the
+    robot to perform the desired car commands (inverse kinematics).    
+
+    Configuration:
+        ~gain (:obj:`float`): scaling factor applied to the desired velocity, default is 1.0
+        ~trim (:obj:`float`): trimming factor that is typically used to offset differences in the
+            behaviour of the left and right motors, it is recommended to use a value that results in
+            the robot moving in a straight line when forward command is given, default is 0.0
+        ~baseline (:obj:`float`): the distance between the two wheels of the robot, default is 0.1
+        ~radius (:obj:`float`): radius of the wheel, default is 0.0318
+        ~k (:obj:`float`): motor constant, assumed equal for both motors, default is 27.0
+        ~limit (:obj:`float`): limits the final commands sent to the motors, default is 1.0
+        ~v_max (:obj:`float`): limits the input velocity, default is 1.0
+        ~omega_max (:obj:`float`): limits the input steering angle, default is 8.0
+
+    """
+
+    def __init__(self) -> None:
+        
+        # Get the vehicle name
+        self.veh_name = rospy.get_namespace().strip("/")
+
+        # Read parameters from a robot-specific yaml file if such exists
+        # self.read_params_from_calibration_file()
+
+        self._k = rospy.get_param("~k")
+
+        # may apply dynamic-reconfiguration to the following parameters
+        self._gain      = rospy.get_param("~gain")
+        self._trim      = rospy.get_param("~trim")
+        self._limit     = rospy.get_param("~limit")
+        self._baseline  = rospy.get_param("~baseline") 
+        self._radius    = rospy.get_param("~radius")
+        self._v_max     = rospy.get_param("~v_max")
+        self._omega_max = rospy.get_param("~omega_max")
+
+        # Setup publishers
+        self.pub_wheels_cmd = rospy.Publisher(
+            "wheels_cmd", WheelsCmd, queue_size=1
+        )    
+        # Setup subscribers
+        self.sub_car_cmd = rospy.Subscriber("cmd_vel", Twist, self.car_cmd_callback)
+        # ---
+        rospy.loginfo("Kinematics Node Initialized, Standing by for twist command")
+
+    def car_cmd_callback(self, msg_car_cmd:Twist):
+
+        #msg_car_cmd.linear.x    = self.trim(msg_car_cmd.linear.x, low=-self._v_max, high=self._v_max)
+        #msg_car_cmd.angular.z   = self.trim(msg_car_cmd.angular.z, low=-self._omega_max, high=self._omega_max)
+
+        msg_car_cmd.linear.x    = max(min(msg_car_cmd.linear.x,self._v_max),-self._v_max)
+        msg_car_cmd.angular.z   = max(min(msg_car_cmd.angular.z,self._omega_max),-self._omega_max)
+
+        # assuming same motor constants k for both motors
+        k_r = k_l = self._k
+
+        # adjusting k by gain and trim
+        k_r_inv = (self._gain + self._trim) / k_r
+        k_l_inv = (self._gain - self._trim) / k_l
+
+        #
+        omega_r = (msg_car_cmd.linear.x + 0.5 * msg_car_cmd.angular.z * self._baseline) / self._radius
+        omega_l = (msg_car_cmd.linear.x - 0.5 * msg_car_cmd.angular.z * self._baseline) / self._radius
+
+        # u_r = omega_r * k_r_inv
+        # u_l = omega_l * k_l_inv
+
+        # limiting output to limit, which is 1.0 for the duckiebot
+        # u_r_limited = self.trim(u_r, -self._limit, self._limit)
+        # u_l_limited = self.trim(u_l, -self._limit, self._limit)
+        # u_r_limited = max(min(u_r,self._limit),-self._limit)
+        # u_l_limited = max(min(u_l,self._limit),-self._limit)
+
+        msg_wheels_cmd              = WheelsCmd()
+        msg_wheels_cmd.vel_right    = omega_r
+        msg_wheels_cmd.vel_left     = omega_l
+        # if msg_car_cmd.linear.x == 0:
+        #     print('kinematic control',omega_l,omega_r)
+        self.pub_wheels_cmd.publish(msg_wheels_cmd)
+
+    # def trim(value,low,high):
+    #     return max(min(value, high), low)
+    
+if __name__ == "__main__":
+    rospy.init_node("kinematics_node")
+    node = KinematicsNode()
+    rospy.spin()
---
+++ scripts/wheel_driver.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import rospy
+from dagu_wheels_driver import DaguWheelsDriver
+
+from std_msgs.msg import Bool
+from vlpduckie_base_control.msg import WheelsCmd,WheelEncoderStamped
+from vlpduckie_base_control.cfg import omegaConfig
+from dynamic_reconfigure.server import Server
+class WheelsDriverNode():
+    """Node handling the motor velocities communication.
+
+        The emergency flag is `False` by default.
+
+    Subscribers:
+       ~wheel_cmd (:obj:`self.defined`)
+       ~emergency_stop (:obj:`Bool`): Emergency stop. Can stop the actual execution of
+           the wheel commands by the motors if set to `True`. Set to `False` for nominal
+           operations.
+    """
+
+    def __init__(self):
+        
+        self.estop = False
+
+        # Setup the driver
+        self.driver = DaguWheelsDriver()
+        #self.log("Initialized.")
+        
+
+        
+        self._left_omega      = 0
+        self._right_omega     = 0
+        self._vel_left_ref    = 0
+        self._vel_right_ref   = 0
+        
+        self._vel_left        = 0
+        self._vel_right       = 0
+        
+        self._err_int         = 0
+        
+        self.kp = 0.08
+        self.ki = 0.0003
+        
+        # Subscribers
+        self.sub_topic = rospy.Subscriber("wheels_cmd", WheelsCmd, self.wheels_cmd_cb, queue_size=1)
+        self.sub_e_stop = rospy.Subscriber("emergency_stop", Bool, self.estop_cb, queue_size=1)
+        self.sub_left_tick  = rospy.Subscriber("left_wheel_encoder_node/tick",WheelEncoderStamped,self.left_omega_callback,queue_size=1)
+        self.sub_right_tick = rospy.Subscriber("right_wheel_encoder_node/tick",WheelEncoderStamped,self.right_omega_callback,queue_size=1)
+        self.srv = Server(omegaConfig,self.dynamic_reconfigure_callback)
+        rospy.loginfo("Wheel Driver Initialized")
+        
+    def wheels_cmd_cb(self, msg:WheelsCmd):
+        """
+        Callback that sets wheels' speeds.
+        """
+        if self.estop:
+            self.driver.set_wheels_speed(left=0, right=0)
+        else:
+            self._vel_left_ref    = msg.vel_left
+            self._vel_right_ref   = msg.vel_right
+
+            # vel_left    = self.omega_pid(self._left_omega,vel_left_ref)
+            # vel_right   = self.omega_pid(self._right_omega,vel_right_ref)
+        
+        
+        # it is likely we also do the latency check here, but for now it is ignored
+    def dynamic_reconfigure_callback(self,config,level):
+        self.kp = config.kp
+        self.ki = config.ki
+        return config
+        
+    def estop_cb(self,msg):
+        """
+        Callback that enables/disables emergency stop
+
+            Args:
+                msg (BoolStamped): emergency_stop flag
+        """
+        self.estop = msg.data
+        if self.estop:
+            self.log("Emergency Stop Activated")
+        else:
+            self.log("Emergency Stop Released")
+
+    def on_shutdown(self):
+        """
+        Shutdown procedure.
+
+        Publishes a zero velocity command at shutdown.
+        """
+        self.driver.set_wheels_speed(left=0.0, right=0.0)
+    
+    def left_omega_callback(self,tick_msg:WheelEncoderStamped):
+        self._left_omega    = tick_msg.omega
+        self._vel_left      = self.omega_pid(self._left_omega,self._vel_left_ref)
+        self.driver.set_wheels_speed(left=self._vel_left, right=self._vel_right)
+        
+        
+    def right_omega_callback(self,tick_msg:WheelEncoderStamped):
+        self._right_omega = tick_msg.omega
+        self._vel_right   = self.omega_pid(self._right_omega,self._vel_right_ref)
+        self.driver.set_wheels_speed(left=self._vel_left, right=self._vel_right)
+
+    def omega_pid(self,cur_omega,cur_ref_omega):
+        if cur_ref_omega < 0.01:
+            return 0
+        kp = self.kp
+        ki = self.ki
+        err_omega = cur_ref_omega - cur_omega
+        self._err_int += err_omega
+        
+        #print('current_err',err_omega)
+        throttle_control = kp * err_omega + ki * self._err_int
+        if throttle_control > 1:
+            throttle_control = 1
+        if throttle_control < -1:
+            throttle_control = -1
+        return throttle_control
+    
+if __name__ == "__main__":
+    rospy.init_node("wheel_driver")
+    node = WheelsDriverNode()
+    rospy.spin()
