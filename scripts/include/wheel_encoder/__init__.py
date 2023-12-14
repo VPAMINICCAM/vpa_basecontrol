3f9ec80a39c245d142a4d291e246b01cacb7dd83
YikaiZengTUD <yikai.zeng@tu-dresden.de>
2023-12-14 16:51:47 +0100

    'fork' from gitee

--- scripts/include/wheel_encoder/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The wheel encoder package contains a single class (i.e., WheelEncoderDriver) that is responsible
-for talking to a single wheel encoder.
-
-.. autoclass:: wheel_encoder.WheelEncoderDriver
-
-"""
-from .wheel_encoder_driver import WheelEncoderDriver, WheelDirection
